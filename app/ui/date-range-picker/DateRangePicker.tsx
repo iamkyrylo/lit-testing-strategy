3e219ef178d076b973f7f0d7ff45c3a537012dbb
Kyrylo Hrishchenko <khrishchenko@cargurus.com>
2026-07-25 14:29:45 +0300

    refactor: reduce DatePicker's text field size

--- app/ui/date-range-picker/DateRangePicker.tsx
+++ app/ui/date-range-picker/DateRangePicker.tsx
@@ -12,6 +12,7 @@ export function DateRangePicker({ from, to, onChange }: DateRangePickerProps) {
             onChange({ from: newFrom, to });
           }
         }}
+        slotProps={{ textField: { size: 'small' }}}
       />
       <DatePicker
         label="To"
@@ -21,6 +22,7 @@ export function DateRangePicker({ from, to, onChange }: DateRangePickerProps) {
             onChange({ from, to: newTo });
           }
         }}
+        slotProps={{ textField: { size: 'small' }}}
       />
     </Stack>
   );
